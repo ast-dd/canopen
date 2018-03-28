de70b43335b16dac0bba8f73c855cd223c3dab6a
Stephan Klatt <stephan.klatt@ast.de>
2018-03-28 13:36:51 +0200

    TEST sdo download: use socket instead of buffer

--- sdo/download_test.go
+++ sdo/download_test.go
@@ -1,43 +1,46 @@
 package sdo
 
 import (
-	"bytes"
 	"github.com/ast-dd/can"
 	"github.com/ast-dd/canopen"
 	"log"
 	"testing"
+	"syscall"
 )
 
 type downloadReadWriteCloser struct {
-	buf bytes.Buffer
+	writeFd int
 }
 
 func (rw *downloadReadWriteCloser) Read(b []byte) (n int, err error) {
-	return rw.buf.Read(b)
+	panic("Read() shouldn't be called anymore")
 }
 
+// write expected response instead of given data
 func (rw *downloadReadWriteCloser) Write(b []byte) (n int, err error) {
-	var frm can.Frame
-	if err := can.Unmarshal(b, &frm); err != nil {
+	var (
+		sendFrm can.Frame
+		respFrm can.Frame
+	)
+	if err := can.Unmarshal(b, &sendFrm); err != nil {
 		return 0, err
 	}
 
-	switch frm.Data[0] & TransferMaskCommandSpecifier {
-		frm = downloadInitiateFrame
+	switch sendFrm.Data[0] & TransferMaskCommandSpecifier {
 	case ClientInitiateDownload:
+		respFrm = downloadInitiateFrame
 	case ClientSegmentDownload:
-		frm = downloadSegmentFrame
+		respFrm = downloadSegmentFrame
 	default:
 		log.Fatal("Unknown command")
 		break
 	}
-	if b, err := can.Marshal(frm); err == nil {
-		return rw.buf.Write(b)
-	} else {
-		return 0, err
+	if response, err := can.Marshal(respFrm); err == nil {
+		err = syscall.Sendmsg(rw.writeFd, response, nil, nil, 0)
+		n = len(b)
 	}
 
-	return len(b), nil
+	return
 }
 
 func (rw *downloadReadWriteCloser) Close() error { return nil }
@@ -69,7 +72,11 @@ var downloadSegmentFrame = can.Frame{
 }
 
 func TestDownload(t *testing.T) {
-	rwc := can.NewReadWriteCloser(&downloadReadWriteCloser{})
+	pair, err := syscall.Socketpair(syscall.AF_UNIX, syscall.SOCK_DGRAM, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rwc := can.NewReadWriteCloser(&downloadReadWriteCloser{writeFd: pair[0]}, pair[1])
 	bus := can.NewBus(rwc)
 
 	go bus.ConnectAndPublish()
@@ -84,7 +91,7 @@ func TestDownload(t *testing.T) {
 		Data: []byte{0x2, 0x57, 0x52, 0x49, 0x54, 0x45, 0x91},
 	}
 
-	err := download.Do(bus)
+	err = download.Do(bus)
 	if err != nil {
 		t.Fatal(err)
 	}
